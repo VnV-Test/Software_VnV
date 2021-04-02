d9f4ce662c1f8db2e76b2f97335527d5290a3346
Cyan00ffff <jungseoyun4@gmail.com>
2021-04-02 13:25:00 +0900

    test중

--- vnvTest/src/testMain/Main.java
+++ vnvTest/src/testMain/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Now testing intellij...");
-        System.out.println("Error")
+        System.out.println("Error");
+        System.out.println("깃 알람 오는지 수정중");
     }
 }
