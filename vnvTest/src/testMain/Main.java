11ca5cf7e9eba962886390631d172eefbbee4133
Cyan00ffff <jungseoyun4@gmail.com>
2021-04-05 20:40:08 +0900

    VMP-3 commit

--- vnvTest/src/testMain/Main.java
+++ vnvTest/src/testMain/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Now testing intellij...");
         System.out.println("Error");
-        System.out.println("깃 알람 오는지 수정중");
+        //System.out.printlgradln("깃 알람 오는지 수정중");
 
 
     }
