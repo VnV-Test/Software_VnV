6cfba825b1135b32ae881a5dfd67ab1fddb5a233
Cyan00ffff <jungseoyun4@gmail.com>
2021-04-08 15:13:30 +0900

    junit test

--- src/main/java/VMProject.java
+++ src/main/java/VMProject.java
@@ -1,7 +1,21 @@
+import java.util.*;
+
 public class VMProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Jira 이슈 연동 테스트");
+        /*System.out.println("Hello world");
+        System.out.println("Jira 이슈 연동 테스트");*/
+        Addnumber addnumber = new Addnumber();
+
+        System.out.println(addnumber.add(2,6));
+        System.out.println(addnumber.add(2,3,4));
+    }
+
+    public Member join(){
+        Member member = new Member();
+        member.setName("VNV");
+        member.setId((long) 1.0);
+
+        return member;
     }
 }
