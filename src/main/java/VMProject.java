3008c303868298dac790f24ce918baae560766a1
Cyan00ffff <jungseoyun4@gmail.com>
2021-04-26 14:41:25 +0900

    commit2

--- src/main/java/VMProject.java
+++ src/main/java/VMProject.java
@@ -3,19 +3,17 @@ import java.util.*;
 public class VMProject {
 
     public static void main(String[] args) {
-        /*System.out.println("Hello world");
-        System.out.println("Jira 이슈 연동 테스트");*/
-        Addnumber addnumber = new Addnumber();
+        int[] ans;
+        int a = 0;
+        if(a>0){
+            //something happens...
+        }
+        else{
+            
+        }
 
-        System.out.println(addnumber.add(2,6));
-        System.out.println(addnumber.add(2,3,4));
-    }
-
-    public Member join(){
-        Member member = new Member();
-        member.setName("VNV");
-        member.setId((long) 1.0);
-
-        return member;
     }
 }
+
+//최희정 최영선 신유진 이승민 박가은 정서윤
+//이진원 이상문
