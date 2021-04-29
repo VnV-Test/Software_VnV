008d40af7607429692fc28406832d6d13001a220
Cyan00ffff <jungseoyun4@gmail.com>
2021-04-29 14:16:45 +0900

    add test codes

--- src/main/java/VMProject.java
+++ src/main/java/VMProject.java
@@ -13,6 +13,15 @@ public class VMProject {
             n1=n2;
             n2=n3;
         }
+
+        int rows = 5;
+
+        for (int i = 1; i <= rows; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
     }
 
 
