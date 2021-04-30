a08a5f14533bf92fd168ccf43bd2a0381bee5d60
Jionee <49470328+Jionee@users.noreply.github.com>
2021-04-30 11:54:05 +0900

    i => k feature

--- src/main/java/VMProject.java
+++ src/main/java/VMProject.java
@@ -3,27 +3,25 @@ import java.util.*;
 public class VMProject {
 
     public static void main(String[] args) {
-        //bug issue testing
-        //only annotation
-        //Google Checks
-        //
-        ////
-        if (1 > 0) {
-            System.out.println("bigger");
-        } else {
-            System.out.println("smaller");
+        int n1=0,n2=1,n3,i,count=10;
+        System.out.print(n1+" "+n2);//printing 0 and 1
+
+        for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed
+        {
+            n3=n1+n2;
+            System.out.print(" "+n3);
+            n1=n2;
+            n2=n3;
         }
 
-        int[] arr;
-        double a = 3;
+        int rows = 5;
 
-
-        if (a > 2) {
-            System.out.println("a equal 3!");
-            //something
-            //dk djEJgrp cnrkgodiehlsmswl 1eh ahfmrpT
-
-        }
+        for (int k = 1; k <= rows; ++k) {
+            for (int j = 1; j <= k; ++j) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        } 
     }
 
 
