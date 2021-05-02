93aeff7f6d5e30169b5abe35a1499abda3e1993f
Jionee <49470328+Jionee@users.noreply.github.com>
2021-05-02 16:15:53 +0900

    Update VMProjectTest.java

--- src/test/java/VMProjectTest.java
+++ src/test/java/VMProjectTest.java
@@ -5,23 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VMProjectTest {
 
-    @Test
-    void multiply(){
-        Addnumber addnumber = new Addnumber();
-        assertEquals(addnumber.add(8,9),17);
-    }
+//     @Test
+//     void multiply(){
+//         Addnumber addnumber = new Addnumber();
+//         assertEquals(addnumber.add(8,9),17);
+//     }
 
-    @Test
-    void 빼기(){
-        Addnumber addnumber = new Addnumber();
-        assertEquals(addnumber.sub(10,9),1);
-    }
+//     @Test
+//     void 빼기(){
+//         Addnumber addnumber = new Addnumber();
+//         assertEquals(addnumber.sub(10,9),1);
+//     }
     
-    @Test
-    void 빼기2(){
-        Addnumber addnumber = new Addnumber();
-        assertEquals(addnumber.sub(10,3),7);
-    }
+//     @Test
+//     void 빼기2(){
+//         Addnumber addnumber = new Addnumber();
+//         assertEquals(addnumber.sub(10,3),7);
+//     }
 
 
 
