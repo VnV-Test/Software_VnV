74c72d6e042f4109714ebcf080d07b1d21b06cb6
eucalyptwos <2468abcde@naver.com>
2021-05-07 11:32:50 +0900

    admin, code 클래스 구현.

---
+++ src/main/java/Admin.java
@@ -0,0 +1,22 @@
+public class Admin {
+    private String adminID;
+    private String adminPW;
+    private String contact;
+
+    public boolean checkIDPW(String id,String pw){
+        return (adminID.equals(id)&&adminPW.equals(pw));
+    }
+    public void editContact(String c){
+        contact=c;
+    }
+    public Admin(){
+        adminPW="admin";
+        adminID="admin";
+        contact="010-0000-0000";
+    }
+    public Admin(String i, String p, String c){
+        adminPW=p;
+        adminID=i;
+        contact=c;
+    }
+}
---
+++ src/main/java/Code.java
@@ -0,0 +1,24 @@
+public class Code {
+    private int code;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCode() {
+        return code;
+    }
+    public boolean confirmCOde(int c){
+        return c==code;
+    }
+
+    public Code(){
+        code=0;
+        name=null;
+    }
+    public Code(int c, String n){
+        code = c;
+        name = n;
+    }
+}
