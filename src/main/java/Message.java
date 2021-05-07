d3e1e31ef928e0279e224f83bd714adbb15f2b63
Sünghjöp Nam <moollyska@gmail.com>
2021-05-07 12:40:06 +0900

    Test입니다.

---
+++ src/main/java/Message.java
@@ -0,0 +1,10 @@
+public class Message {
+    public int src_id;
+    public int validity;
+    public int cvc;
+    public int pw;
+
+    public boolean payment(int cardNum){
+        return boolean;
+    }
+}
