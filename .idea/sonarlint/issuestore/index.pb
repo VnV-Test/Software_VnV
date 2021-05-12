777d339e545c32711ce0b22c3f14dedb1bb56fbf
Sünghjöp Nam <moollyska@gmail.com>
2021-05-12 16:02:00 +0900

    Message Protocol added

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,11 +1,3 @@
 
 <
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
-J
-src/main/java/Message.java,7\8\7846c008bbd895e9b88a52d8f43bc66b4310ee6c
-L
-src/main/java/VMProject.java,8\4\840430033e583af81845d86be601677f8f53005b
-G
-src/main/java/Code.java,8\a\8aa794fe4d9deb4565da0ad9ebdf290bcb5a89ee
-H
-src/main/java/Admin.java,2\e\2e7d2d6e6db3bc22aa74f9a1fb2b085e004c9e28
--- src/main/java/TestMain.java
+++ src/main/java/TestMain.java
@@ -1,9 +1,64 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 public class TestMain {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        double[] location = { 32.123 };
-        VM vm = new VM(89268, location);
+        double[] location = { 37.54164, 127.07880 };
+        VM vm = new VM(9999, location);
+
+        Thread t = new RecieveMail(vm);
+        t.start();
+
         MainFrame main = new MainFrame(vm);
+    }
+}
+class RecieveMail extends Thread{
+    public VM vm;
+
+    public RecieveMail(VM v){
+        super("rm");
+        this.vm = v;
+    }
+
+    @Override
+    public void run(){
+        BufferedReader in = null;
+        ServerSocket listener = null;
+        Socket socket = null;
+
+        while(true){
+
+            try{
+                listener = new ServerSocket(vm.getID());
+            } catch(IOException e) {
+                System.out.println(e.getMessage() + "Here??");
+            }
+
+            try{
+                socket = listener.accept();
+                System.out.println("Connected.");
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                String inputMessage = in.readLine();
+
+                String[] tempS = inputMessage.split(",");
+
+                Message mg = new Message(Integer.valueOf(tempS[0]), Integer.valueOf(tempS[1]), Integer.valueOf(tempS[2]), tempS[3]);
+                this.vm.MailRecieve(mg);
+
+            } catch(IOException e){
+                System.out.println(e.getMessage()+"Here??");
+            } finally {
+                try{
+                    socket.close();
+                    listener.close();
+                } catch (IOException e){
+                    System.out.println("Error occured while chatting with client.");
+                }
+            }
+        }
     }
 }
